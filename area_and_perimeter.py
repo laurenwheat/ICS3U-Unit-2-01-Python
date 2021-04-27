43db2a70a8fc7e19c5ee017f7ee2b07e82b20da1
Ubuntu <ubuntu@ip-172-31-59-214.ec2.internal>
2021-04-27 21:55:38 +0000

    determined area and circumfrence

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 3
+
+# Created by Lauren Wheatley
+# Created in April 2021
+# This is the "Hello, World!" program, with proper style
+
+import math
+
+
+def main():
+    # this function calculates the area and perimeter of a circle
+
+    print("Area is {} mm².".format(math.pi*15**2))
+    print("Perimeter is {} mm.".format(2*math.pi*15))
+    print("Done:D")
+
+
+if __name__ == "__main__":
+    main()
